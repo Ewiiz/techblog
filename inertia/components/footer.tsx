6710809b4b0c025491db853574c18b84f08f9524
Ewiiz <128278275+Ewiiz@users.noreply.github.com>
2024-07-29 15:11:00 +0400

    Create footer.tsx

---
+++ inertia/components/footer.tsx
@@ -0,0 +1,27 @@
+export default function Footer() {
+  return (
+    <footer className="flex flex-col p-4 border border-x-0 border-b-0 border-black/10">
+      <div className="mx-auto">
+        <ul className="flex space-x-4">
+          <li>
+            <a href="https://twitter.com" className="hover:underline">
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com" className="hover:underline">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="mailto:someone@example.com" className="hover:underline">
+              Email
+            </a>
+          </li>
+        </ul>
+
+        <div className="mt-4 text-sm">© 2024 Your Company Name</div>
+      </div>
+    </footer>
+  )
+}
